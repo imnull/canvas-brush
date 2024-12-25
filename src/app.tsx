40528895d0242d5b6464a33d6eaf810fbb4a3983
marvin <imnull@outlook.com>
2024-12-25 11:28:46 +0800

    feat: paint

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react"
+import './app.scss'
+
+import {
+    createPaintController,
+    renderPoints,
+} from './canvas-brush'
+
 export default () => {
-    return <h1>Hello</h1>
+    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
+    useEffect(() => {
+        if(!canvas) {
+            return
+        }
+        const handler = createPaintController(canvas, {
+            onMove: (point, privousPoint, points) => {
+                renderPoints(canvas, points)
+            }
+        })
+        return () => {
+            handler.dispose()
+        }
+    }, [canvas])
+    return <div className="canvas-brush-wrapper">
+        <h1>Canvas Brush</h1>
+        <canvas className="canvas-brush" width={640} height={480} ref={setCanvas}></canvas>
+    </div>
 }
